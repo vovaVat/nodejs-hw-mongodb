515db14bb6f427a39c097e603e247788bf12c108
Volodymyr <vovavat97@gmail.com>
2025-03-04 22:02:36 +0200

    hz

--- src/controllers/contactsController.js
+++ src/controllers/contactsController.js
@@ -1,46 +1,39 @@
-const contactsService = require('../services/contacts');
-
-async function getContacts(req, res) {
-  try {
-    const contacts = await contactsService.getAllContacts();
-    res.status(200).json({
-      status: 200,
-      message: 'Successfully found contacts!',
-      data: contacts,
-    });
-  } catch (error) {
-    res.status(500).json({
-      status: 500,
-      message: 'Failed to get contacts.',
-    });
-  }
-}
-
-async function getContactById(req, res) {
-  try {
-    const { contactId } = req.params;
-    const contact = await contactsService.getContactById(contactId);
-
-    if (!contact) {
-      return res.status(404).json({
-        message: 'Contact not found',
+export default {
+  async getContacts(req, res) {
+    try {
+      const contacts = await contactsService.getAllContacts();
+      res.status(200).json({
+        status: 200,
+        message: 'Successfully found contacts!',
+        data: contacts,
+      });
+    } catch (error) {
+      res.status(500).json({
+        status: 500,
+        message: 'Failed to get contacts.',
       });
     }
+  },
 
-    res.status(200).json({
-      status: 200,
-      message: `Successfully found contact with id ${contactId}!`,
-      data: contact,
-    });
-  } catch (error) {
-    res.status(500).json({
-      status: 500,
-      message: 'Failed to get contact.',
-    });
-  }
-}
-
-module.exports = {
-  getContacts,
-  getContactById,
+  async getContactById(req, res) {
+    const { contactId } = req.params;
+    try {
+      const contact = await contactsService.getContactById(contactId);
+      if (!contact) {
+        return res.status(404).json({
+          message: 'Contact not found',
+        });
+      }
+      res.status(200).json({
+        status: 200,
+        message: `Successfully found contact with id ${contactId}!`,
+        data: contact,
+      });
+    } catch (error) {
+      res.status(500).json({
+        status: 500,
+        message: 'Failed to get contact.',
+      });
+    }
+  },
 };
