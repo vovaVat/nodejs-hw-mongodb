814ef30e4e50b6193f9e2189b3c9e06057bad418
Volodymyr <vovavat97@gmail.com>
2025-03-18 20:34:51 +0200

    hz

--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ import express from 'express';
 import dotenv from 'dotenv';
 import cors from 'cors';
 import contactsRouter from './routes/contacts.js';
+import setupServer from './server.js';
 
 dotenv.config();
 
@@ -10,13 +11,4 @@ const app = express();
 
 initMongoConnection();
 
-app.use(cors());
-app.use(express.json());
-
-app.use('/contacts', contactsRouter);
-
-const PORT = process.env.PORT || 3000;
-
-app.listen(PORT, () => {
-  console.log(`Server is running on port ${PORT}`);
-});
+setupServer();
