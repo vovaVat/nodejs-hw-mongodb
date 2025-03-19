ace93bc9dff333fd48f2a584cc0358a51f8a68a2
Volodymyr <vovavat97@gmail.com>
2025-03-19 19:11:35 +0200

    hz

--- src/routes/contacts.js
+++ src/routes/contacts.js
@@ -11,13 +11,6 @@ const router = express.Router();
 
 router.get('/', getContacts);
 
-router.use('/contacts/:id', (req, res, next) => {
-  if (!Types.ObjectId.isValid(req.params.id)) {
-    return next(createError(400, 'Invalid contact ID format'));
-  }
-  next();
-});
-
 router.get('/contacts/:id', getContactById);
 
 router.post('/', addContact);
