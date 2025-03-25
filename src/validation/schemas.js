e33853d78b5f19560f515e8f4eb0d9716292039b
Volodymyr <vovavat97@gmail.com>
2025-03-25 23:06:44 +0200

    hz

--- src/validation/schemas.js
+++ src/validation/schemas.js
@@ -3,13 +3,17 @@ import Joi from 'joi';
 export const contactSchema = Joi.object({
   name: Joi.string().min(3).max(20).required(),
   email: Joi.string().email().required(),
-  phone: Joi.string().min(7).max(15).required(),
+  phone: Joi.string()
+    .pattern(/^\d{10,15}$/)
+    .required(),
   isFavourite: Joi.boolean(),
 });
 
 export const updateContactSchema = Joi.object({
   name: Joi.string().min(3).max(20),
   email: Joi.string().email(),
-  phone: Joi.string().min(7).max(15),
+  phone: Joi.string()
+    .pattern(/^\d{10,15}$/)
+    .required(),
   isFavourite: Joi.boolean(),
 }).or('name', 'email', 'phone', 'isFavourite');
