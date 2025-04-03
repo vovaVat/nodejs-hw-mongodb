9b51acf24e9a043d54f663664b417bd5fedecda7
Volodymyr <vovavat97@gmail.com>
2025-04-03 22:37:07 +0300

    hz

--- src/middlewares/authenticate.js
+++ src/middlewares/authenticate.js
@@ -11,7 +11,7 @@ const authenticate = async (req, res, next) => {
     }
 
     const token = authHeader.split(' ')[1];
-    const decoded = jwt.verify(token, process.env.ACCESS_TOKEN_SECRET);
+    const decoded = jwt.verify(token, process.env.JWT_SECRET);
 
     if (!decoded) {
       throw createError(401, 'Access token expired');
